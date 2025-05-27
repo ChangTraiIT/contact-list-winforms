d4c6d82f4145bd582d687faa9a7ae53144aeaba7
Quoc Dep Trai <leanhquoc0-511.04.com>
2025-05-27 09:41:18 +0700

    Thêm Form để thêm liên hệ mới

---
+++ ContactList/AddContactForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ContactList
+{
+    partial class AddContactForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblHoTen;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label lblSoDienThoai;
+        private System.Windows.Forms.TextBox txtSoDienThoai;
+        private System.Windows.Forms.Button btnLuu;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.lblSoDienThoai = new System.Windows.Forms.Label();
+            this.txtSoDienThoai = new System.Windows.Forms.TextBox();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.Location = new System.Drawing.Point(30, 30);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(55, 16);
+            this.lblHoTen.TabIndex = 0;
+            this.lblHoTen.Text = "Họ tên:";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(120, 27);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(200, 22);
+            this.txtHoTen.TabIndex = 1;
+            // 
+            // lblSoDienThoai
+            // 
+            this.lblSoDienThoai.AutoSize = true;
+            this.lblSoDienThoai.Location = new System.Drawing.Point(30, 70);
+            this.lblSoDienThoai.Name = "lblSoDienThoai";
+            this.lblSoDienThoai.Size = new System.Drawing.Size(89, 16);
+            this.lblSoDienThoai.TabIndex = 2;
+            this.lblSoDienThoai.Text = "Số điện thoại:";
+            // 
+            // txtSoDienThoai
+            // 
+            this.txtSoDienThoai.Location = new System.Drawing.Point(120, 67);
+            this.txtSoDienThoai.Name = "txtSoDienThoai";
+            this.txtSoDienThoai.Size = new System.Drawing.Size(200, 22);
+            this.txtSoDienThoai.TabIndex = 3;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(120, 110);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(100, 30);
+            this.btnLuu.TabIndex = 4;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // AddContactForm
+            // 
+            this.ClientSize = new System.Drawing.Size(360, 170);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.txtSoDienThoai);
+            this.Controls.Add(this.lblSoDienThoai);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.lblHoTen);
+            this.Name = "AddContactForm";
+            this.Text = "Thêm liên hệ mới";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ ContactList/AddContactForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ContactList
+{
+    public partial class AddContactForm : Form
+    {
+        public AddContactForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string ten = txtHoTen.Text.Trim();
+            string sdt = txtSoDienThoai.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(ten) || string.IsNullOrWhiteSpace(sdt))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ họ tên và số điện thoại.");
+                return;
+            }
+
+            MessageBox.Show($"Liên hệ đã lưu:\nHọ tên: {ten}\nSĐT: {sdt}", "Thông báo");
+            this.Close();
+        }
+    }
+}
